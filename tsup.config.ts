def091a388ed20851d84334d059aefb12cdc3276
Patrick Miller <pamiller.pdx@gmail.com>
2022-12-28 11:36:46 +0900

    Fix package json

--- tsup.config.ts
+++ tsup.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
           'version',
           'dependencies',
           'peerDependencies',
+          'peerDependenciesMeta',
           'repository',
           'keywords',
           'author',
